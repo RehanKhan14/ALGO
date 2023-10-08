cc42e3f3433d9f8f59a692419b3d20dd137f5fee
Shayan <s.m.shayanhussain@gmail.com>
2023-10-09 01:10:09 +0500

    homepage structure + content

--- geometric-algorithms/src/components/Hero.tsx
+++ geometric-algorithms/src/components/Hero.tsx
@@ -1,26 +1,38 @@
-import { Grid } from "@mui/material";
-import myImage from './images/placeholder2.jpg'
+import { Grid, Divider, Button } from "@mui/material";
+import style from "./css/Hero.module.css"
+import { redirect, useNavigate } from "react-router-dom";
+
 interface algo{
     heading:string;
-    imgs:string[]
+    imgs:string[];
+    description:string;
 }
 
 export const Hero = (props:algo) => {
+    const navigate=useNavigate();
+    const validUrl=`/${props.heading.toLowerCase().replace(" ","-")}`;
    return (
-    <div>
-        <Grid container spacing={0}>
+    <div className={style.container}>
+        <Grid container >
   
             <Grid xs={3}>
+                <div className={style.textContainer}>
                 <h3>{props.heading}</h3>
-                <p>Example Images</p>
-                <button>Run Algorithm</button>
+                <p>{props.description}</p>
+                <Button variant="contained" size="large" onClick={()=>{
+                    navigate(validUrl);
+                }}>Learn More</Button>
+                </div>
             </Grid>
             <Grid xs={9}>
-                <div>{props.imgs.map((value:string)=>(
-                    (<img src={`./images/${value}.jpg`} alt="example" width="200px" height="200px"/>)
-                ))}</div>
+                <div className={style.imgContainer}>
+                    {props.imgs.map((value:string)=>(
+                    (<img src={`./images/${value}.jpg`} alt="example"/>)
+                ))}
+                </div>
                 
             </Grid>
+                <Divider textAlign="right" variant="inset" className={style.endline}/>
         </Grid>
     </div>
    ); 
--- geometric-algorithms/src/pages/Home.tsx
+++ geometric-algorithms/src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { Hero } from "../components/Hero";
-interface HeroDetails{
-    heading:string,
-    imgs:string[3]
-}
 
 export const Home = () =>{
     return (
         <>
-            <Hero heading={"Convex Hull"} imgs={["placeholder1", "placeholder2", "placeholder3"]}/>
+            <Hero heading={"Closest Pair"} imgs={["placeholder1", "placeholder2", "placeholder3"]} description="The closest pair problem involves finding the two closest points in a set of points in a two-dimensional or higher-dimensional space."/>
+
+            <Hero heading={"Convex Hull"} imgs={["placeholder1", "placeholder2", "placeholder3"]} description="In mathematics, the convex hull of a set X of points in a Euclidean space is the smallest convex set that contains X."/>
+
+            <Hero heading={"Voronoi Diagram"} imgs={["placeholder1", "placeholder2", "placeholder3"]} description="A Voronoi diagram is a geometric structure that partitions a space into regions based on the closest proximity to a set of input points."/>
         </>
     );
 }
