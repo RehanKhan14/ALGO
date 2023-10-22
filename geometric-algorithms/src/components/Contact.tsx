3a59dbb815b1ee3e652514eb6f388f7a9fadf4f7
Muhammad Rehan Khan <muhammadrehankhan000@gmail.com>
2023-10-22 20:18:06 +0500

    added styling and content in contact us

--- geometric-algorithms/src/components/Contact.tsx
+++ geometric-algorithms/src/components/Contact.tsx
@@ -13,7 +13,7 @@ export const Contact = () =>{
             <h1>Developer Details</h1>
             <Grid container className={style.cards}>
                 <Grid xs={3} className={style.card}>
-                    <img src="./images/placeholder1.jpg" alt="person"/>
+                    <img src="./images/shayan.jpg" alt="person"/>
                     <p>
                         <b>Name: </b>Syed Muhammad Shayan Hussain<br/> 
                         <b>Email: </b>s.m.shayanhussain@gmail.com<br/> 
@@ -22,18 +22,18 @@ export const Contact = () =>{
                     </p>
                 </Grid>
                 <Grid xs={3} className={style.card}>
-                    <img src="./images/placeholder1.jpg" alt="person"/>
+                    <img src="./images/rehan.jpg" alt="person"/>
                     <p>
-                        <b>Name: </b>Syed Muhammad Shayan Hussain<br/> 
-                        <b>Email: </b>s.m.shayanhussain@gmail.com<br/> 
-                        <b>LinkedIn: </b>Syed Muhammad Shayan Hussain<br/>
+                        <b>Name: </b>Muhammad Rehan Khan<br/> 
+                        <b>Email: </b>muhammadrehankhan000@gmail.com<br/> 
+                        <b>LinkedIn: </b>https://www.linkedin.com/in/rehan-khan-b540b51b7/<br/>
                         <b>University: </b>FAST NUCES, Karachi<br/>
                     </p>
                 </Grid>
                 <Grid xs={3} className={style.card}>
-                    <img src="./images/placeholder1.jpg" alt="person"/>
+                    <img src="./images/azan.jpeg" alt="person"/>
                     <p>
-                        <b>Name: </b>Syed Muhammad Shayan Hussain<br/> 
+                        <b>Name: </b>Azan Nabi Khan<br/> 
                         <b>Email: </b>s.m.shayanhussain@gmail.com<br/> 
                         <b>LinkedIn: </b>Syed Muhammad Shayan Hussain<br/>
                         <b>University: </b>FAST NUCES, Karachi<br/>
